56ad92d0c93b3e170b4c386e3826cf290aaac865
jasonmpittman <jpittman@cs.highpoint.edu>
2019-10-14 11:02:43 -0400

    added logic to handle config and urls files

--- syllogis.py
+++ syllogis.py
@@ -23,22 +23,39 @@ def read_config():
     return config
 
 # read url file into collection
-def read_urls():
-    with open('urls.json') as urls_file:
+def read_urls(urls_file):
+    with open(urls_file) as urls_file:
         urls = json.load(urls_file)
 
     return urls
 
-def download_pdf():
-    url = 'https://arxiv.org/abs/1910.04872'
-    link = url.replace('/abs/', '/pdf/')
+def download_pdf(urls, dir):
+    for url in urls:
+        link = url.replace('/abs/', '/pdf/')
+
+        # need to replace open() with filename
+        with libreq.urlopen(link) as response, open('1910.04872.pdf', 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+
+    #url = 'https://arxiv.org/abs/1910.04872'
+    #link = url.replace('/abs/', '/pdf/')
 
     #with libreq.urlopen("https://arxiv.org/pdf/1910.04872") as response, open('1910.04872.pdf', 'wb') as out_file:
     #    shutil.copyfileobj(response, out_file)
 
 
+def main():
+    config = read_config()
+    url_file = config['url_file']['path'] + '/' + config['url_file']['name']
+    download_dir = config['downloads']['path'] + '/' + config['downloads']['folder']
+    
+    #print(download_dir)
+    
+    urls = read_urls(url_file)
+    print(urls)
+    #download_pdf(urls)
 
 
-
-
-
+if __name__ == "__main__":
+    main()
